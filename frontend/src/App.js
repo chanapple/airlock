5f8eb3137e64d59594b89f88347f4fa03dcfc22e
HyeJinYoon1226 <younzzippong@gmail.com>
2025-11-12 17:55:48 +0900

    [front] ChatRoom import 주석처리로 ESLint 오류 해결

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import ChatRoom from "./components/chat/ChatRoom";
+// import ChatRoom from "./components/chat/ChatRoom";
 import ChatContainer from "./components/chat/ChatContainer";
 
 function App() {
